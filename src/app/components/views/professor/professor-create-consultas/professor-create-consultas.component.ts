69965b0cd17477b0b1c01e2cb893e96980d06c7c
FirminoAndrade <103321080+FirminoAndrade@users.noreply.github.com>
2023-06-03 20:42:54 -0300

    atalizacao 03-06-2023 20:42

--- src/app/components/views/professor/professor-create-consultas/professor-create-consultas.component.ts
+++ src/app/components/views/professor/professor-create-consultas/professor-create-consultas.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Consulta } from 'src/app/components/models/consulta.modelo';
 import { ClienteService } from 'src/app/components/services/cliente.service';
 import { ConsultaService } from 'src/app/components/services/consulta.service';
+import { UsuarioService } from 'src/app/components/services/usuario.service';
 
 @Component({
   selector: 'app-professor-create-consultas',
@@ -23,29 +24,44 @@ export class ProfessorCreateConsultasComponent implements OnInit{
     observacao: ""
   }
 
-  id_cliente: string = "";
-  nome!: String
+  id_cliente!: string;
+  nomeCliente!: string
+  id_usuario!: string ;
+  nomeUsuario!: string;
+  matricula!: string
  constructor(
     private serviceConsulta: ConsultaService,
     private serviceCliente: ClienteService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private service: UsuarioService
   ) {}
 
   ngOnInit(): void {
+    this.id_usuario = this.service.usuario && this.service.usuario.id ?  this.service.usuario.id : '';
     this.id_cliente = this.route.snapshot.paramMap.get("id_cliente")!;
-    this.nome = this.route.snapshot.paramMap.get("nome")!;
-    this.buscarNome();
+    this.nomeCliente = this.route.snapshot.paramMap.get("nome")!;
+    this.buscarNomeCliente();
+    this.buscarNomeUsuario();
   }
 
-  buscarNome (): void{
+  buscarNomeCliente(): void{
   this.serviceCliente.buscarPorId(this.id_cliente).subscribe((reposta) => {
-    this.nome = reposta.nome;
+    this.nomeCliente = reposta.nome;
   });
+  }
+
+  buscarNomeUsuario(): void {
+    this.service.buscarPorId(this.id_usuario).subscribe((reposta) => {
+      this.nomeUsuario = reposta.nome;
+      this.matricula = reposta.matricula;
+    });
   }
 
 
   public criarConsulta(): void {
+    this.consulta.matricula = this.matricula;
+    this.consulta.nomeAtendente = this.nomeUsuario;
     this.serviceConsulta.criarConsultaService(this.id_cliente, this.consulta).subscribe(
       (resposta) => {
         this.router.navigate(["professor/clientes/" + this.id_cliente + "/consultas"]);
